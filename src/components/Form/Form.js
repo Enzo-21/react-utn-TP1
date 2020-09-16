332f4b57283592e491bf1c1d24945ab66dcfe18f
Enzo-21 <enzo21barrera@gmail.com>
2020-09-15 21:49:20 -0300

    First commit

---
+++ src/components/Form/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//Styles
+import './Form.scss'
+
+const Form = () => {
+    return (
+        <div className='form-container'>
+            Form Component
+        </div>
+    )
+}
+
+export default Form
